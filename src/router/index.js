22878ff01f9cc37cdf722f75364a4abb44521558
asaf-y-tech <asaf.y@utopia.tech>
2023-12-14 15:38:12 +0200

    updates

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../components/Home.vue';
 import Login from '../components/Login.vue';
 import HelloWorld from '../components/HelloWorld.vue';
+import HelloWorldTwo from '../components/HelloWorldTwo.vue';
 import AppTest from '../components/AppTest.vue';
 
 
@@ -12,7 +13,8 @@ const routes = [
   { path: '/', component: Home },
   { path: '/login', component: Login },
   { path: '/HelloWorld', component: HelloWorld },
-  { path: '/AppTest', component: AppTest }
+  { path: '/AppTest', component: AppTest },
+  { path: '/HelloWorldTwo', component: HelloWorldTwo },
   // Add more routes as needed
 ];
 
